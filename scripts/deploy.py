518ab737c3143de53eb40d366342130324ca5e58
Odiase <odiaseefosa18@gmail.com>
2022-12-23 06:49:10 +0100

    Written deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,5 @@
 # third party packages imports
-from brownie import network, accounts, config
-from brownie import 
+from brownie import network, accounts, config, CrowdFundFactory
 
 #local imports
 from .helpers import get_account
@@ -8,3 +7,8 @@ from .helpers import get_account
 
 def deploy_contract():
     account = get_account()
+
+    # deploying contract
+    contract_transaction = CrowdFundFactory.deploy({"from" : account})
+    contract_transaction.wait(1)
+    return contract_transaction
